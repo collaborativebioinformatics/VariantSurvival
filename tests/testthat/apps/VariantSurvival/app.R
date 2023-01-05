41e9304087bf766159c5bc4b56e93ff6dd413ff4
Thomas Krannich <thomas@Thomass-MacBook-Pro-2.local>
2023-01-05 14:28:41 +0100

    delete app tests as project structure curently does not comply with shiny testing standard

--- tests/testthat/apps/VariantSurvival/app.R
+++
@@ -1,4 +0,0 @@
-
-library(VariantSurvival)
-VariantSurvival::VariantSurvival(vcffile      = "VariantSurvival_package/R/merged.filtered.vcf",
-                                 metadatafile = "VariantSurvival_package/R/metadata.xlsx")
