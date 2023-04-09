e736bcff5800b0230c8061b8a9be6cd317db690e
Hiba Ben Aribi <73958439+HibaBenAribi@users.noreply.github.com>
2023-04-09 15:51:04 +0100

    Delete VariantSurvival_package directory

--- VariantSurvival_package/R/getGeneName.R
+++
@@ -1,8 +0,0 @@
-#' `getGeneName`
-#' @param info info
-#' @return gene id
-#' @noRd
-getGeneName <- function(info, geneIDS) {
-  x <- str_extract(info['INFO'], "(?<=ensembl_gene_id=)[^;]+")
-  return(geneIDS[x,]$GeneName)
-}
